ec1d8612217956d8bce104b39093ddd589ffceac
Mihail <mcoscodan@ranorex.com>
2022-06-17 18:44:56 +0300

    Add Department, Employee and PaymentMode models

---
+++ EShopSystem/EShopSystem/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EShopSystem.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public bool DepartmentIsActive { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ EShopSystem/EShopSystem/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EShopSystem.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeMobile { get; set; }
+        public string EmployeeEmail { get; set; }
+        public int DepartmentId { get; set; }
+        public virtual Department Department { get; set; }
+    }
+}
