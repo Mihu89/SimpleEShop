53c329ca73d15c91b485865680b022cf316cdedf
Mihail <mcoscodan@ranorex.com>
2022-06-17 19:00:37 +0300

    Add DbContext and connection String

---
+++ EShopSystem/EShopSystem/DAL/ShopDbContext.cs
@@ -0,0 +1,30 @@
+﻿using EShopSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace EShopSystem.DAL
+{
+    public class ShopDbContext: DbContext
+    {
+        public ShopDbContext() : base("ShopConnection")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<PaymentMode> PaymentModes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // remove convention for pluralise table name
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
